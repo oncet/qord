3fadf02b8a26d7fb530e52bc827eb76f67603634
Camilo Rivera <crivera@srpago.com>
2021-10-14 23:04:40 -0300

    use home as menu page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Container, Heading, Text } from "@chakra-ui/react";
+import {
+  Container,
+  Heading,
+  HStack,
+  Icon,
+  Image,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 
 import Establishment from "./pages/Establishment";
 import Layout from "./components/Layout";
+import { FaMinus, FaPlus, FaTimes } from "react-icons/fa";
 
 function App() {
   return (
@@ -12,39 +21,46 @@ function App() {
         <Router>
           <Switch>
             <Route path="/" exact>
-              <Heading>Homepage</Heading>
-              <Text>
-                Dolor ipsum ullamco labore irure Lorem fugiat sunt officia enim.
-                Pariatur quis eiusmod non deserunt deserunt nostrud labore
-                dolore consectetur sunt. Mollit ullamco nostrud excepteur
-                laboris ex pariatur Lorem laborum nostrud consequat nostrud non
-                dolor quis. Eiusmod nisi et eiusmod veniam tempor nulla laborum
-                excepteur.
-              </Text>
-              <Text>
-                Dolor ipsum ullamco labore irure Lorem fugiat sunt officia enim.
-                Pariatur quis eiusmod non deserunt deserunt nostrud labore
-                dolore consectetur sunt. Mollit ullamco nostrud excepteur
-                laboris ex pariatur Lorem laborum nostrud consequat nostrud non
-                dolor quis. Eiusmod nisi et eiusmod veniam tempor nulla laborum
-                excepteur.
-              </Text>
-              <Text>
-                Dolor ipsum ullamco labore irure Lorem fugiat sunt officia enim.
-                Pariatur quis eiusmod non deserunt deserunt nostrud labore
-                dolore consectetur sunt. Mollit ullamco nostrud excepteur
-                laboris ex pariatur Lorem laborum nostrud consequat nostrud non
-                dolor quis. Eiusmod nisi et eiusmod veniam tempor nulla laborum
-                excepteur.
-              </Text>
-              <Text>
-                Dolor ipsum ullamco labore irure Lorem fugiat sunt officia enim.
-                Pariatur quis eiusmod non deserunt deserunt nostrud labore
-                dolore consectetur sunt. Mollit ullamco nostrud excepteur
-                laboris ex pariatur Lorem laborum nostrud consequat nostrud non
-                dolor quis. Eiusmod nisi et eiusmod veniam tempor nulla laborum
-                excepteur.
-              </Text>
+              <Heading>Menú</Heading>
+              <VStack
+                background="gray.900"
+                borderRadius="md"
+                px="2"
+                pt="1"
+                pb="2"
+                minHeight="9em"
+                justifyContent="space-between"
+                alignItems="left"
+              >
+                <Heading mb="0" size="lg">
+                  Hamburguesa Manush
+                </Heading>
+                <Image src="/images/burguer.jpg" alt="Segun Adebayo" />
+                <HStack justifyContent="space-between">
+                  <HStack spacing="3">
+                    <HStack
+                      alignItems="baseline"
+                      fontSize="2xl"
+                      letterSpacing="wide"
+                      spacing="1"
+                    >
+                      <Icon as={FaTimes} boxSize="4" />
+                      <strong>2</strong>
+                    </HStack>
+                    <Text
+                      as="span"
+                      fontSize="xl"
+                      fontWeight="bold"
+                      color="gray.400"
+                    >
+                      $1200
+                    </Text>
+                  </HStack>
+                  <Text fontSize="2xl">
+                    <strong>$600</strong>
+                  </Text>
+                </HStack>
+              </VStack>
             </Route>
             <Route path="/:establishment">
               <Establishment />
